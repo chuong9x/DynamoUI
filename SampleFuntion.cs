c3ee189be53be6035ba9689dfdda3b794947597d
chuong9x <31106432+chuong9x@users.noreply.github.com>
2020-03-17 13:12:58 +0700

    re

---
+++ SampleFuntion.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamoUI.Funtion
+{
+    public static class DynamoFuntion
+    {
+        public static double MultiplyTwoNumbers(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
